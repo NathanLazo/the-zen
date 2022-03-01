aa7663ff38331ce548020a5aeabb0e4e064ab227
Nathan Lazo <nathanlazo@outlook.com>
2022-03-01 15:52:38 -0700

    SVG fixed

--- components/dropdownMenu.js
+++ components/dropdownMenu.js
@@ -42,7 +42,7 @@ export default function Example() {
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="inline-flex icon icon-tabler icon-tabler-user mr-[.40rem]" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="inline-flex icon icon-tabler icon-tabler-user mr-[.40rem]" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="12" cy="7" r="4" />
                     <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
@@ -59,7 +59,7 @@ export default function Example() {
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg"className="inline-flex icon icon-tabler icon-tabler-user mr-[.40rem]" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg"className="inline-flex icon icon-tabler icon-tabler-user mr-[.40rem]" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
                     <circle cx="12" cy="12" r="3" />
